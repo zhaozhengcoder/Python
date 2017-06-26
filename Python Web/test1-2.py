8eeee1a66ad14bbf78bfba81925254651d4ac9ca
赵政 <www.a821400729@qq.com>
2017-06-26 15:11:28 +0800

    Add files via upload

--- Python Web/test1-2.py
+++ Python Web/test1-2.py
@@ -16,18 +16,21 @@ def page_default():
 def application(environ,start_response):
     #print (environ)
     start_response('200 OK',[('Content-type','text/html')])
+    method = environ['REQUEST_METHOD']
     #environ是一个map，environ['PATH_INFO']输出的是 /index.py, 而environ['PATH_INFO'][1:]输出的是 index.py,少掉了 /
-    body = '<h1>Hello, %s !</h1>' % (environ['PATH_INFO'][1:] or 'web')
 
-    
-    if(environ['PATH_INFO']=='/'):
-        return page_index()
-    if(environ['PATH_INFO']=='/login'):
-        return page_login()
-    else:
-        return page_default()
-    return [body.encode('utf-8')]
+    if(method=='GET'):    
+        if(environ['PATH_INFO']=='/'):
+            return page_index()
+        if(environ['PATH_INFO']=='/login'):
+            return page_login()
+        else:
+            return page_default()
+    if(method=='POST'):
+        html='method is post'
+        return [html.encode('utf-8')]
 
 httpd=make_server('',8100,application)
 print ('serving http on port 8100 ... ')
+
 httpd.serve_forever()
